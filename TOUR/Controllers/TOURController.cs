2cb2efc12473e2ff5e2990ac662ffaf62271c71a
nguyendat2k <74171813+nguyendat2k@users.noreply.github.com>
2021-05-29 10:46:29 -0400

    Initial commit

---
+++ TOUR/Controllers/TOURController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TOUR.Models;
+
+namespace TOUR.Controllers
+{
+    public class TOURController : Controller
+    {
+        QLDatTourEntities db = new QLDatTourEntities();
+        // GET: TOUR
+        private List<Tour> LayTourMoi(int soluong)
+        {
+            return db.Tours.OrderByDescending(n => n.MaTour).ToList();
+        }
+        public ActionResult Index()
+        {
+            return View(LayTourMoi(6));
+        }
+        public ActionResult Details(string id)
+        {
+            var tour = (from s in db.Tours
+                        where s.MaTour == id
+                        select new ViewTour()
+                        {
+                            matour = s.MaTour,
+                            tentour = s.TenTour,
+                            mota = s.MoTa,
+                            songay = s.SoNgay,
+                            gia = s.Gia,
+                            ngaykhoihanh = (System.DateTime)s.NgayKhoiHanh,
+                            anhbia = s.Anhbia
+                        }).SingleOrDefault();
+            return View(tour);
+        }    
+    }
+}
---
+++ TOUR/Models/ViewTour.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TOUR.Models
+{
+    public class ViewTour
+    {
+        public string matour { get; set; }
+        public string tentour { get; set; }
+        public string mota { get; set; }
+        public byte? songay { get; set; }
+        public double? gia { get; set; }
+        public DateTime ngaykhoihanh { get; set; }
+        public string anhbia { get; set; }
+
+    }
+}
