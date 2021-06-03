0a34528c865e4cef24755b014626416ba94701cc
nguyendat2k <74171813+nguyendat2k@users.noreply.github.com>
2021-06-03 11:30:08 -0400

    khach san

--- TOUR/Controllers/KHACHSANController.cs
+++ TOUR/Controllers/KHACHSANController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TOUR.Models;
 
 namespace TOUR.Controllers
 {
     public class KHACHSANController : Controller
     {
+        QLDatTourEntities db = new QLDatTourEntities();
+
         // GET: KHACHSAN
+        private List<KhachSan> LayKhachSanMoi(int soluong)
+        {
+            return db.KhachSans.OrderByDescending(n => n.MaKS).ToList();
+        }
         public ActionResult Index()
         {
-            return View();
+            return View(LayKhachSanMoi(10));
+        }
+        public ActionResult Details(string id)
+        {
+            var ks = (from s in db.KhachSans
+                        where s.MaKS == id
+                        select new ViewKhachSan()
+                        {
+                            maks = s.MaKS,
+                            tenks = s.TenKS,
+                            diachiks = s.DiaChiKS,
+                            anhks = s.AnhKS,
+                            mahang = s.MaHang,                         
+                        }).SingleOrDefault();
+            return View(ks);
         }
     }
 }
---
+++ TOUR/Models/ViewKhachSan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TOUR.Models
+{
+    public class ViewKhachSan
+    {
+        public string maks { get; set; }
+        public string tenks { get; set; }
+        public string diachiks { get; set; }
+        public string anhks { get; set; }
+        public string mahang { get; set; }
+    }
+}
