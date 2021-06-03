084cafa473aaa95c21dadd9c6ac106f2cfd3773d
nguyendat2k <74171813+nguyendat2k@users.noreply.github.com>
2021-06-03 10:15:48 -0400

    1

---
+++ TOUR/Controllers/KHACHSANController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TOUR.Controllers
+{
+    public class KHACHSANController : Controller
+    {
+        // GET: KHACHSAN
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
